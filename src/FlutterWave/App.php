ab62a989211d8f31f64d77d33ebf0ac8d60da40f
denisdevke <deniskem.dev@gmail.com>
2024-03-07 00:05:02 +0300

    added the app class

---
+++ src/FlutterWave/App.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author Techlup Solutions
+ */
+
+namespace Techlup\FlutterWave; 
+
+final class App{
+
+    /**
+     * @var String the secret key used to authenticate flutter-wave API
+     */
+    protected String $secret_key;
+
+    /**
+     * Constructs a new App class
+     * 
+     * @var String $secret_key the secret key used to authenticate flutter-wave API
+     */
+    public function __construct(String $secret_key) {
+        $this->secret_key = $secret_key;
+    }
+    
+}
