f8ac8c4c4c30ab78cf637d65156def28267eb755
denisdevke <deniskem.dev@gmail.com>
2024-03-07 00:26:52 +0300

    created the payments class

---
+++ src/FlutterWave/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @author Techlup Solutions
+ */
+
+namespace Techlup\FlutterWave;
+
+final class Constants{
+    const PAYMENTS_ENDPOINT = 'https://api.flutterwave.com/v3/payments';
+}
